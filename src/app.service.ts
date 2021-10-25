3cf4fbca132804c5dc9bddacd457ef6cb602ea2d
Yasio <baszczok@yasio.pl>
2021-10-25 10:43:22 +0200

    add bagde with satus

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,6 +20,7 @@ export class AppService {
       };
     }
     const targetPath = path.resolve(__dirname, process.env.TARGET_DIR);
+    const cdnPath = path.resolve(__dirname, '../cdn');
     try {
       process.chdir(targetPath);
 
@@ -30,6 +31,8 @@ export class AppService {
         };
       }
 
+      fs.copyFileSync(`${cdnPath}/building.png`, `${cdnPath}/status.png`);
+
       // Enter your command here
       const child = spawn('npm', ['run', 'build'], { shell: true });
       this.isBuilding = true;
@@ -38,6 +41,7 @@ export class AppService {
         const response = data.toString();
         if (response.indexOf('Done') > -1 || response.indexOf('Error') > -1) {
           this.isBuilding = false;
+          fs.copyFileSync(`${cdnPath}/deployed.png`, `${cdnPath}/status.png`);
         }
         if (response.toLowerCase().indexOf('error') > -1) {
           this.logger.error(response);
@@ -45,6 +49,7 @@ export class AppService {
         this.logger.log(response);
       });
     } catch (err) {
+      fs.copyFileSync(`${cdnPath}/none.png`, `${cdnPath}/status.png`);
       this.logger.error(err);
       return {
         message: err,
